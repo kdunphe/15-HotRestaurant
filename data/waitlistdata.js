8bda0d61720d74e26fab65bde61a144dea63dfa1
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-08-29 11:31:22 -0400

    Adding table & waitlist data arrays

---
+++ data/waitlistdata.js
@@ -0,0 +1,10 @@
+const waitlistArray = [
+    {
+        customerName: "Hilary",
+        customerId: "354",
+        customerEmail: "hilary@example.com",
+        customerPhone: "555-789-1234"
+    }
+];
+
+module.exports = waitlistArray;
--- server.js
+++ server.js
@@ -3,6 +3,29 @@ const express = require("express");
 const app = express()
 const PORT = 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+
+
+
+
 
 
 app.listen(PORT, function() {
